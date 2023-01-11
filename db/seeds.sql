13eb0bd38dc482117f3ba45503358a4ac600ccb7
Aidan <aidanreynolds715@gmail.com>
2023-01-11 10:32:04 -0500

    addition of SQL code and files

---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+INSERT into department (name) 
+VALUES 
+('Opperations'),
+('Production'),
+('Finance'),
+('Legal')
+('Engineering');
+
+INSERT into role (title, salary, department_id)
+VALUES
+('Cleaning Manager', 50000, 1),
+('Production Manager', 65000, 2),
+('Account Manager', 85000, 3),
+('Lawyer', 100000, 4);
+('Lead Engineer', 150000, 5);
+
+insert into employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Ricky ', 'Bobby', 3, null),
+('Dale', 'Doback', 1, 2),
+('Jackie', 'Moon', 5, 3),
+('Ron', 'Burgundy', 3, null);
+('Allen', 'Gamble', 4, 5);
